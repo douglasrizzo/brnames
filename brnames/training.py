bdd35531e4d6aab3368cd1621351731fe32bd2dd
Douglas <douglasrizzom@gmail.com>
2023-03-10 04:49:00 -0300

    implement differences in single run for when inside and outside of tune

--- brnames/training.py
+++ brnames/training.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from random import choice
 from typing import Any, Dict
 
 import pytorch_lightning as pl
 import ray
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 from ray import air, tune
+from pytorch_lightning.callbacks.progress.rich_progress import RichProgressBar
 from ray.air.integrations.wandb import WandbLoggerCallback
 from ray.tune.integration.pytorch_lightning import TuneReportCheckpointCallback
 from ray.tune.schedulers import ASHAScheduler
@@ -15,25 +16,42 @@ from .model import Transformer
 
 
 WANDB_PROJECT_NAME = "brnames"
-WANDB_GROUP_NAME = "tune"
+WANDB_GROUP_NAMES = {"tune": "tune", "standalone": "standalone"}
 
 
-def train_single(config: Dict[str, Any], data_path: Path, max_epochs: int) -> None:
+def train_single(
+    config: Dict[str, Any], data_path: Path, max_epochs: int, in_tune: bool = False
+) -> None:
     datamodule = NGramDataModule(data_path, 512, 8)
     model = Transformer(config)
+
+    callbacks: list[pl.Callback] = [
+        EarlyStopping("Loss/Val", 0.001, 30),
+    ]
+    logger = False
+    # if using Tune, create its own callback, otherwise configure Lightning loggers
+    if in_tune:
+        callbacks.append(
+            TuneReportCheckpointCallback(["Loss/Val", "Loss/Train"], on="validation_end")
+        )
+    else:
+        callbacks.append(RichProgressBar())
+        logger = (
+            WandbLogger(project=WANDB_PROJECT_NAME, group=WANDB_GROUP_NAMES["standalone"])
+            if config["wandb"]
+            else TensorBoardLogger(save_dir=".")
+        )
+
     # tune automatically logs the values listed in TuneReportCallback, so we don't configure loggers to Lightning
     trainer = pl.Trainer(
-        logger=False,
+        logger=logger,
         accelerator="gpu",
         max_epochs=max_epochs,
         val_check_interval=0.5,
         precision=16,
         auto_scale_batch_size=True,
-        enable_progress_bar=False,
-        callbacks=[
-            TuneReportCheckpointCallback(["Loss/Val", "Loss/Train"], on="validation_end"),
-            EarlyStopping("Loss/Val", 0.001, 30),
-        ],
+        enable_progress_bar=not in_tune,
+        callbacks=callbacks,
     )
     trainer.tune(model, datamodule=datamodule)
     trainer.fit(model, datamodule=datamodule)
@@ -44,12 +62,13 @@ def train_tune(train_config: Dict[str, Any], param_space: Dict[str, Any]):
         train_single,
         data_path=train_config["datapath"],
         max_epochs=train_config["max_epochs"],
+        in_tune=True,
     )
     scheduler = ASHAScheduler(max_t=train_config["max_epochs"], grace_period=1, reduction_factor=2)
     # configure wandb callback if chosen, logging to TensorBoard is done automatically by tune
     ray_callbacks = (
         [
-            WandbLoggerCallback(project=WANDB_PROJECT_NAME, group=WANDB_GROUP_NAME),
+            WandbLoggerCallback(project=WANDB_PROJECT_NAME, group=WANDB_GROUP_NAMES["tune"]),
         ]
         if train_config["wandb"]
         else []
@@ -58,7 +77,7 @@ def train_tune(train_config: Dict[str, Any], param_space: Dict[str, Any]):
     tuner = tune.Tuner(
         tune.with_resources(
             train_fn_with_parameters,
-            resources={"cpu": 8, "gpu": 1},
+            resources={"cpu": 8, "gpu": 1},  # resources for a single task
         ),
         tune_config=tune.TuneConfig(
             metric="Loss/Val",
